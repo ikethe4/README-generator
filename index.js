7aab92f91c46341e39a7a8b9b708b391a3cdb38e
Isaac Arnold <isaac@iaivphotography.com>
2020-10-15 21:23:37 -0500

    slight grammer changes in questions, README2 changed to sampleREADME.

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const fs = require("fs");
 const util = require("util");
 const writeFile = util.promisify(fs.writeFile);
 // array of questions for user
-const questions = ["What is the title of this project?", "What does this project do?", "What is the installation instructions?  Any needed dependencies?", "Who contributed to this project", "What are the instructions to test this project?", "How is this to be used?"
+const questions = ["What is the title of this project?", "What does this project do?", "What are the installation instructions?  Any needed dependencies?", "What are the github usernames of the contributors?", "What are the instructions to test this project?", "How is this to be used?"
 ];
 
 const license = ["Apache-License-2.0", "BSD-3-Clause", "BSD-2-Clause", "GNU-General-Public-License-(GPL)", "GNU-Library-General-Public License-(LGPL)", "MIT-license", "Mozilla-Public-License-2.0", "Common-Development-and-Distribution-License", "Eclipse-Public-Licenseversion-2.0"]
@@ -53,7 +53,7 @@ async function init() {
 
     ]);  console.log(data)
     // writeToFile("README2", generateMarkdown(data))
-    writeFile("README2.md", generateMarkdown(data))
+    writeFile("sampleREADME.md", generateMarkdown(data))
 
   } catch (err){
       console.log(err)
---
+++ sampleREADME.md
@@ -0,0 +1,26 @@
+# README Generator
+
+  ## Description
+  This project seeks to streamline the README writing process by generating a well formatted README through a series of prompts in console.
+  ## Table of Contents
+  * [Installation](#installation)
+  * [Contributors](#contributors)
+  * [Test](#test)
+  * [Instructions](#test)
+  * [License](#license)
+  
+  ## Installation
+  This program does require node and inquirer.
+
+  ## Contributors
+  [ikethe4](https://github.com/ikethe4)
+
+  ## Test
+  This project does not currently have test parameters.
+
+  ## Instructions
+  The user will answer a series of prompts in console.  Those responses will be passed into a mardown generator function that will format the responses into a well formatted README.
+
+  ## License
+  ![license](https://badgen.net/badge/:license/:Apache-License-2.0/:color?icon=github)
+
